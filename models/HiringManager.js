b52bfb4d87d613465d20ca2456a1c76bfed4ee7f
Tasnim Pricila <tasnimtanzim55@gmail.com>
2022-11-07 01:37:23 +0600

    candidate and manager model created

---
+++ models/HiringManager.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
+
+const hiringManagerSchema = mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: [true, "Name is required"],
+        minLength: [3, "Name must be at least 3 characters."],
+        maxLength: [50, "Name is too large."],
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: [true, "Email must be unique"],
+        required: [true, "Email is required"],
+        validate: [validator.isEmail, "Please provide a valid mail."]
+    },
+    contactNumber: {
+        type: String,
+        trim: true,
+        required: [true, "contactNumber is required"],
+        validate: [validator.isMobilePhone, "Please provide a valid phone number"]
+    },
+    address: {
+        type: String,
+        trim: true,
+        required: [true, "Job Requirements is required"],
+    },
+    imageUrl: {
+        type: String,
+        trim: true,
+        required: [true, "Location is required"],
+        validate: [validator.isURL, "Please provide a valid Url"]
+    },
+    postedJobs: [{
+        id: {
+            type: ObjectId,
+            ref: "Job"
+        }
+    }]
+},{
+    timeStamps: true
+})
+
+const HiringManager = mongoose.model("HiringManager", hiringManagerSchema);
+module.exports = HiringManager;
--- models/Job.js
+++ models/Job.js
@@ -86,7 +86,7 @@ const jobSchema = mongoose.Schema({
         id: {
             type: ObjectId,
             ref: "HiringManager",
-            // required: true
+            required: true
         },
     },
     appliedBy: [{
